84c9a35c6b738f51e5aad3239d1d71eb9a849fe4
Jan T. Sott <git@idleberg.com>
2022-07-14 15:45:42 +0200

    move back assets to layout

--- src/routes/__layout-wp.svelte
+++ src/routes/__layout-wp.svelte
@@ -22,6 +22,14 @@
 	});
 </script>
 
+<svelte:head>
+  <link rel="stylesheet" id="dashicons-css" href={`wp-includes/css/dashicons.min.css?ver=${import.meta.env.VITE_WORDPRESS_VERSION}`} type="text/css" media="all" />
+  <link rel="stylesheet" id="buttons-css" href={`wp-includes/css/buttons.min.css?ver=${import.meta.env.VITE_WORDPRESS_VERSION}`} type="text/css" media="all" />
+  <link rel="stylesheet" id="forms-css" href={`wp-admin/css/forms.min.css?ver=${import.meta.env.VITE_WORDPRESS_VERSION}`} type="text/css" media="all" />
+  <link rel="stylesheet" id="l10n-css" href={`wp-admin/css/l10n.min.css?ver=${import.meta.env.VITE_WORDPRESS_VERSION}`} type="text/css" media="all" />
+  <link rel="stylesheet" id="login-css" href={`wp-admin/css/login.min.css?ver=${import.meta.env.VITE_WORDPRESS_VERSION}`} type="text/css" media="all" />
+</svelte:head>
+
 {#if !isLoading}
 	<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
