fdc5ba7fc49b51a913f89eee19dae9734113a00a
idleberg <git@idleberg.com>
2022-07-29 12:07:53 +0200

    group directory actions

--- tools/bootstrap.mjs
+++ tools/bootstrap.mjs
@@ -5,12 +5,14 @@ shell.exec('npx degit wordpress/wordpress .wordpress || echo "WordPress folder e
 shell.cp('.wordpress/wp-includes/images/w-logo-blue-white-bg.png', 'static/favicon.png');
 
 shell.mkdir('-p', 'static/wp-includes/css');
-shell.mkdir('-p', 'static/wp-admin/css');
-shell.mkdir('-p', 'static/wp-admin/images');
 shell.cp('.wordpress/wp-includes/css/dashicons.min.css', 'static/wp-includes/css/dashicons.min.css');
 shell.cp('.wordpress/wp-includes/css/buttons.min.css', 'static/wp-includes/css/buttons.min.css');
+
+shell.mkdir('-p', 'static/wp-admin/css');
 shell.cp('.wordpress/wp-admin/css/forms.min.css', 'static/wp-admin/css/forms.min.css');
 shell.cp('.wordpress/wp-admin/css/l10n.min.css', 'static/wp-admin/css/l10n.min.css');
 shell.cp('.wordpress/wp-admin/css/login.min.css', 'static/wp-admin/css/login.min.css');
+
+shell.mkdir('-p', 'static/wp-admin/images');
 shell.cp('.wordpress/wp-admin/images/w-logo-blue.png', 'static/wp-admin/images/w-logo-blue.png');
 shell.cp('.wordpress/wp-admin/images/wordpress-logo.svg', 'static/wp-admin/images/wordpress-logo.svg');
