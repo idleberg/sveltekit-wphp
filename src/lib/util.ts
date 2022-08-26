10ba8b54211e959d3a55a9a3e62618309ce6e26d
Jan T. Sott <git@idleberg.com>
2022-08-26 11:22:31 +0200

    add multiplier for development mode

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,9 @@
 export function randomResponse(min = 10, max = 2000) {
-	return Math.floor(Math.random() * (max - min + 1) + min);
+	const multiplier = isDev() ? 0.1 : 1;
+
+	return Math.floor(Math.random() * (max - min + 1) + min) * multiplier;
+}
+
+export function isDev() {
+	return import.meta.env.MODE === 'development';
 }
